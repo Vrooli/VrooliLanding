b7ef7a2d4315e641f4e1ae46e204243ddb19b3ff
Matthew Halloran <mdhalloran@yahoo.com>
2022-02-13 22:55:41 -0500

    Revert "Lazy load fix"
    This reverts commit 7693da4bfc194c133fee16d82bbe01a47ca3142d.

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -4,15 +4,17 @@ import { Route, Switch } from 'wouter';
 import { LANDING_LINKS as LINKS } from 'utils/consts';
 import { BusinessFields } from 'utils/consts';
 import { ScrollToTop } from 'components';
-import { Page } from 'pages/wrapper/Page';
 
 // Lazy loading in the Routes component is a recommended way to improve performance. See https://reactjs.org/docs/code-splitting.html#route-based-code-splitting
-const { AboutPage } = lazily(() => import('./pages/informational/AboutPage'));
-const { HomePage } = lazily(() => import('./pages/informational/HomePage'));
-const { MissionPage } = lazily(() => import('./pages/informational/MissionPage'));
-const { NotFoundPage } = lazily(() => import('./pages/NotFoundPage'));
-const { PrivacyPolicyPage } = lazily(() => import('./pages/informational/PrivacyPolicyPage'));
-const { TermsPage } = lazily(() => import('./pages/informational/TermsPage'));
+const {
+    AboutPage,
+    HomePage,
+    MissionPage,
+    NotFoundPage,
+    Page,
+    PrivacyPolicyPage,
+    TermsPage,
+} = lazily(() => import('./pages'));
 
 const Routes = () => {
 
@@ -25,47 +27,35 @@ const Routes = () => {
                 {/* ========= START INFORMATIONAL ROUTES ========= */}
                 {/* Informational pages to describe Vrooli to potential customers */}
                 <Route path={LINKS.Home}>
-                    <Suspense fallback={<div>Loading...</div>}>
-                        <Page title={title('Home')}>
-                            <HomePage />
-                        </Page>
-                    </Suspense>
+                    <Page title={title('Home')}>
+                        <HomePage />
+                    </Page>
                 </Route>
                 <Route path={LINKS.Mission}>
-                    <Suspense fallback={<div>Loading...</div>}>
-                        <Page title={title('Mission')}>
-                            <MissionPage />
-                        </Page>
-                    </Suspense>
+                    <Page title={title('Mission')}>
+                        <MissionPage />
+                    </Page>
                 </Route>
                 <Route path={LINKS.About}>
-                    <Suspense fallback={<div>Loading...</div>}>
-                        <Page title={title('About')}>
-                            <AboutPage />
-                        </Page>
-                    </Suspense>
+                    <Page title={title('About')}>
+                        <AboutPage />
+                    </Page>
                 </Route>
                 <Route path={LINKS.PrivacyPolicy}>
-                    <Suspense fallback={<div>Loading...</div>}>
-                        <Page title={title('Privacy Policy')}>
-                            <PrivacyPolicyPage />
-                        </Page>
-                    </Suspense>
+                    <Page title={title('Privacy Policy')}>
+                        <PrivacyPolicyPage />
+                    </Page>
                 </Route>
                 <Route path={LINKS.Terms}>
-                    <Suspense fallback={<div>Loading...</div>}>
-                        <Page title={title('Terms & Conditions')}>
-                            <TermsPage />
-                        </Page>
-                    </Suspense>
+                    <Page title={title('Terms & Conditions')}>
+                        <TermsPage />
+                    </Page>
                 </Route>
                 {/* ========= END INFORMATIONAL ROUTES ========= */}
                 <Route>
-                    <Suspense fallback={<div>Loading...</div>}>
-                        <Page title={title('404')}>
-                            <NotFoundPage />
-                        </Page>
-                    </Suspense>
+                    <Page title={title('404')}>
+                        <NotFoundPage />
+                    </Page>
                 </Route>
             </Switch>
         </Suspense>
