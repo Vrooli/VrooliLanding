7b5d4d970cb27e85211b4f57fe39b8c6d5c686ee
MattHalloran <matthalloran8@gmail.com>
2022-10-28 00:46:02 +0000

    Server updates p1
    Added scripts to improve setup and deployment process, which were taken from main repo. Also added custom route package, since it will be needed for sitemap generation

---
+++ packages/shared/route/src/index.ts
@@ -0,0 +1,4 @@
+export * from './matcher';
+export * from './router';
+export * from './sitemap';
+export * from './useLocation';
---
+++ scripts/setup.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+# Sets up NPM, Yarn, global dependencies, and anything else 
+# required to get the project up and running.
+HERE=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+source "${HERE}/prettify.sh"
+
+header "Checking for package updates"
+sudo apt-get update
+header "Running upgrade"
+sudo apt-get -y upgrade
+
+header "Installing nvm"
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+source ~/.nvm/nvm.sh
+
+header "Installing Node (includes npm)"
+nvm install 16.16.0
+nvm alias default v16.16.0
+
+header "Installing Yarn"
+npm install -g yarn
+
+header "Installing global dependencies"
+yarn global add typescript ts-node nodemon react-scripts serve
+
+header "Installing local dependencies"
+cd "${HERE}/.." && yarn cache clean && yarn
+
+"${HERE}/shared.sh"
+
+# header "Combining node_modules from all packages into one"
+
+info "Done! You may need to restart your editor for syntax highlighting to work correctly." 
+info "If you haven't already, copy .env-example to .env and edit it to match your environment."
